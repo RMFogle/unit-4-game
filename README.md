6fe0f3936d291c5479388e056c1a812858c99011
RyFogle <13fogle@gmail.com>
2019-04-09 16:39:01 -0700

    final push for hw

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # unit-4-game
 week #4 assignment
+
+How to play Crystals Collector: 
+
+*The player or user is trying to match the random generated number displayed next to "Match this number" 
+
+1. Player clicks on one of four crystals/buttons. These crystals are given random values at the start of each turn until the player wins or losses. 
+2. When player clicks on one of four crystals/buttons the hidden value is added to "Your total score is" 
+3. The player wins by matching their total score to the "Match this number" 
+4. The player losses by having their total score exceed the "Match this number" 
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
 // Generate random number to be displayed when the page loads. 
 // Random number should be between 19-120. 
 var randomNumber = Math.floor(Math.random() * 120) + 19;  
-$('#match-number').html("<h2>Match this number: </h2>" + randomNumber); 
+$('#match-number').html("<h2>Match this number:  </h2>" + randomNumber); 
 console.log("Match this number: " + randomNumber); 
 
 
 // Generate random numbers for each crystal. 
 // Random number should be between 1-12.
-buttonOne = Math.floor(Math.random() * 12) + 1;
-buttonTwo = Math.floor(Math.random() * 12) + 1;
-buttonThree = Math.floor(Math.random() * 12) + 1;
-buttonFour = Math.floor(Math.random() * 12) + 1; 
+var buttonOne = Math.floor(Math.random() * 12) + 1;
+var buttonTwo = Math.floor(Math.random() * 12) + 1;
+var buttonThree = Math.floor(Math.random() * 12) + 1;
+var buttonFour = Math.floor(Math.random() * 12) + 1; 
 console.log("btn one: " + buttonOne); 
 console.log("btn two: " + buttonTwo);
 console.log("btn three: " + buttonThree);
@@ -20,36 +20,36 @@ console.log("btn four: " + buttonFour);
 
 
 // Create variable for wins, losses and player total score. 
-playerTotal = 0; 
-wins = 0; 
-losses = 0; 
+var playerTotal = 0; 
+var wins = 0; 
+var losses = 0; 
 
 // Create game reset. 
 
 function resetGame() {
     randomNumber = Math.floor(Math.random() * 120) + 19;  
-    $('#match-number').html("<h2>Match this number: </h2>" + randomNumber); 
+    $('#match-number').html("<h2>Match this number:  </h2>" + randomNumber); 
     console.log("Match this number: " + randomNumber); 
     buttonOne = Math.floor(Math.random() * 12) + 1;
     buttonTwo = Math.floor(Math.random() * 12) + 1;
     buttonThree = Math.floor(Math.random() * 12) + 1;
     buttonFour = Math.floor(Math.random() * 12) + 1;
     playerTotal = 0; 
-    $('#total-score').html(playerTotal); 
+    $('#total-score').html("<h3>Your total score is:   </h3>" + playerTotal);
 }
 
 
 // Create a function and or method to add wins to player wins. 
 function playerWins() {
     wins++;
-    $('#wins').html(wins); 
+    $('#wins').html("<h2>Wins:  </h2>" + wins); 
     resetGame();
 }
 
 // Create a function and or method to add losses to player losses. 
 function playerLosses() {
     losses++; 
-    $('#losses').html(losses) 
+    $('#losses').html("<h2>Losses:  </h2>" + losses) 
     resetGame(); 
 }
 
@@ -57,12 +57,65 @@ function playerLosses() {
 // Create a button, click functions for each cyrstal. 4 total. 
 // Should display to player total score each time when clicked.  
 // Create conditionals for win or lose. 
+$('.button-one').on("click", function() {
+    playerTotal += buttonOne; 
+    console.log("Your total score is: " + playerTotal); 
+    // try .fadeToggle for fun? 
+    $('#total-score').html("<h3>Your total score is:   </h3>" + playerTotal); 
+
+    if (playerTotal === randomNumber) {
+        playerWins(); 
+    }
+    else if (playerTotal >= randomNumber) {
+        playerLosses()
+    }
+})
+
+$('.button-two').on("click", function() {
+    playerTotal = playerTotal + buttonTwo; 
+    console.log("Your total score is: " + playerTotal); 
+    // try .fadeToggle for fun? 
+    $('#total-score').html("<h3>Your total score is:   </h3>" + playerTotal); 
+
+    if (playerTotal === randomNumber) {
+        playerWins(); 
+    }
+    else if (playerTotal >= randomNumber) {
+        playerLosses()
+    }
+})
+
+$('.button-three').on("click", function() {
+    playerTotal = playerTotal + buttonThree; 
+    console.log("Your total score is: " + playerTotal); 
+    // try .fadeToggle for fun? 
+    $('#total-score').html("<h3>Your total score is:   </h3>" + playerTotal); 
+
+    if (playerTotal === randomNumber) {
+        playerWins(); 
+    }
+    else if (playerTotal >= randomNumber) {
+        playerLosses()
+    }
+})
+
+$('.button-four').on("click", function() {
+    playerTotal = playerTotal + buttonFour; 
+    console.log("Your total score is: " + playerTotal); 
+    // try .fadeToggle for fun? 
+    $('#total-score').html("<h3>Your total score is:   </h3>" + playerTotal); 
+
+    if (playerTotal === randomNumber) {
+        playerWins(); 
+    }
+    else if (playerTotal >= randomNumber) {
+        playerLosses()
+    }
+});
 
 
 
-
-
-//*Note try .push, .text, .append, or .html to link variables to html id's or classes. 
+//*Note don't know if I need to .push, .text, .append, or .html to link variables to html id's or classes. 
 
 
 }); 
